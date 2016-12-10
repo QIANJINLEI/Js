141a1d65445ae2b8a1e92cbff211f75e6ae91ccb
MR.QIAN <18914010426@163.com>
2016-12-10 23:55:06 +0800

    21312

---
+++ app/src/main/java/com/demo/mrqian/js/MainActivity.java
@@ -0,0 +1,136 @@
+package com.demo.mrqian.js;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.SystemClock;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWv;
+    private Button mBtn;
+    private LocationManager lm;
+    private WebSettings settings;
+
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWv = (WebView) findViewById(R.id.wv);
+        mBtn = (Button) findViewById(R.id.btn);
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mWv.loadUrl("javascript:sum(13,15)");
+            }
+        });
+        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(this, "请开启GPS导航...", Toast.LENGTH_SHORT).show();
+            //返回开启GPS导航设置界面
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivityForResult(intent, 0);
+            return;
+        } else
+
+        {
+
+            String bestProvider = lm.getBestProvider(getCriteria(), true);//获取精度
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            Location lastKnownLocation = lm.getLastKnownLocation(bestProvider);
+
+
+            double longitude = lastKnownLocation.getLongitude();
+            double latitude = lastKnownLocation.getLatitude();
+            System.out.println("longitude" +longitude+"@@@latitude"+latitude);
+
+
+
+        }
+
+
+        settings = mWv.getSettings();
+        settings.setJavaScriptEnabled(true);
+        mWv.loadUrl("file:///android_asset/web.html");
+        //mWebView.loadUrl("javascript:sum(3,8)");
+       // mWv.loadUrl("javascript:sum(3,8)");
+       //mWv.addJavascriptInterface(this,"android");
+        mWv.addJavascriptInterface(this,"aaa");
+
+    /*    mWv.evaluateJavascript("sum(3,8)", new ValueCallback<String>() {
+            @Override
+            public void onReceiveValue(String s) {
+              //  Toast.makeText(getApplicationContext(), "Toast:" + s, Toast.LENGTH_LONG).show();
+            }
+        });*/
+
+    }
+
+
+
+    @JavascriptInterface
+    public void onSumResult(int result) {
+        Log.e("ee", "onSumResult result=" + result);
+    }
+//我是你大爷
+
+    @JavascriptInterface
+    public void toastMessage(String message){
+        Toast.makeText(getApplicationContext(), "通过Natvie传递的Toast:" + message, Toast.LENGTH_LONG).show();
+    }
+
+    public Criteria getCriteria() {
+        Criteria criteria =new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        //设置是否要求速度
+        criteria.setSpeedRequired(false);
+        // 设置是否允许运营商收费
+        criteria.setCostAllowed(false);
+        //设置是否需要方位信息
+        criteria.setBearingRequired(false);
+        //设置是否需要海拔信息
+        criteria.setAltitudeRequired(false);
+        // 设置对电源的需求
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        return criteria;
+    }
+}
